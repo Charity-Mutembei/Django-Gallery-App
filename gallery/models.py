46305801ef0ca6499cc5983f8c919f77314d4ce2
Charity-Mutembei <charity.mutembei@student.moringaschool.com>
2022-05-29 11:11:38 +0300

    Doc: Admin page created, models migrated, and registered into the admin.py file

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,47 @@
+from audioop import maxpp
+from webbrowser import get
 from django.db import models
+from django.http import Http404
 
 # Create your models here.
+class Location(models.Model):
+    name = models.CharField(max_length= 50)
+    def __str__(self):
+        return self.name
+
+class Category(models.Model):
+    name = models.CharField(max_length= 50)
+
+    def __str__(self):
+        return self.name
+
+class Image(models.Model):
+    image = models.ImageField(upload_to = 'galleries', null = False, blank = False)
+    name =models.CharField(max_length= 50)
+    description = models.TextField()
+    location = models.ForeignKey('Location', on_delete=models.DO_NOTHING)
+    category = models.ForeignKey('Category', on_delete=models.DO_NOTHING)
+
+    def save_image(self):
+        '''
+        method to save the image
+        '''
+        self.save()
+    
+    @classmethod
+    def search_by_category(cls, search_term):
+        '''
+        method to search image by its category
+        '''
+        image = cls.objects.filter(category__icontains = search_term)
+        return image
+    @classmethod
+    def filter_by_location(cls, search_term):
+        '''
+        method to filter images by their location
+        '''
+        image = cls.objects.filter(location__icontains = search_term)
+        return image                           
+        
+
+
--- gallery/views.py
+++ gallery/views.py
@@ -1,9 +1,52 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.http import HttpResponse, Http404
+from django.shortcuts import render, redirect
+from .models import Image
+from .forms import ImageForm
 # Create your views here.
 def welcome (request):
     '''
     this is the landing page. Containns the home page of this application
     '''
     return render (request, 'landing.html')
+
+def image (request, pk):
+    try: 
+        image = Image.objects.get(id = pk)
+    
+    except ValueError:
+        raise Http404()
+    
+    return render(request, 'image.html', {'image':image})
+
+def updateImage(request, pk):
+    '''
+    this is a method that allows us to update images
+    '''
+    image = Image.objects.get(id = pk)
+    form = ImageForm(instance = image)
+
+    if request.method == 'POST':
+        form = ImageForm(request.POST, instance = image)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    context = {'form': form}
+    return render(request, 'image_form.html', context)
+
+
+def deleteImage(request, pk):
+    '''
+    method that will delete images 
+    '''
+    image = Image.objects.get(id = pk)
+    
+    if request.method == 'POST':
+        image.delete()
+        return redirect('home')
+    
+    return render(request, 'delete.html', {'obj': image})
+
+
