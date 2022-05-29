4818781e19d78a4eafef47bf95232cd00c6d9f56
Charity-Mutembei <charity.mutembei@student.moringaschool.com>
2022-05-29 14:37:58 +0300

    Doc: Testcase class for the model class Location created and tested and ran okay

--- gallery/models.py
+++ gallery/models.py
@@ -8,7 +8,8 @@ class Location(models.Model):
     name = models.CharField(max_length= 50)
     def __str__(self):
         return self.name
-
+    def save_location(self):
+        self.save()
 class Category(models.Model):
     name = models.CharField(max_length= 50)
 
--- gallery/tests.py
+++ gallery/tests.py
@@ -2,3 +2,27 @@ from django.test import TestCase
 from .models import Location, Category, Image
 
 # Create your tests here.
+
+class LocationTestClass(TestCase):
+    '''
+    class that tests of the class model location
+    '''
+    #setup method
+    def setUp(self):
+        self.name = Location(name = '')
+
+    #testing whether we are being instantiated properly
+
+    def test_instance (self):
+        self.assertTrue(isinstance(self.name, Location))
+
+    #testing the save method
+    def test_save_method(self):
+        self.name.save_location()
+        locations = Location.objects.all()
+        self.assertTrue(len(locations)> 0)
+    
+class ImageTestClass(TestCase):
+    '''
+    test the class model image to ensure the application runs properly
+    '''
